65bda9924eded835994e472e55e32d63dc8d8077
yashh230605 <nangiayash7@gmail.com>
2024-10-08 15:05:30 +0530

    Add files via upload

---
+++ experiment-20c.cpp
@@ -0,0 +1,37 @@
+//Bubble Sort
+#include<iostream>
+using namespace std;
+ 
+void swap(int* a,int* b){
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+int main(){
+    int elements;
+    cout<<"How many elements in the array? :";
+    cin>>elements;
+    int array[elements];
+    cout<<"Enter elements:";
+    for(int i=0;i<elements;i++){
+        cin>>array[i];
+    }
+    for(int i=0;i<elements;i++){
+        cout<<array[i]<<" ";
+    }
+    int n=0;
+    while(n!=elements){
+        for(int i=0;i<elements-n;i++){
+            if(array[i]>array[i+1]){
+                swap(&array[i],&array[i+1]);
+            }
+        }
+        n++;
+    }
+    cout<<"\nSorted array is:"<<endl;
+    for(int i=0;i<elements;i++){
+        cout<<array[i]<<" ";
+    }
+    return 0;
+}
